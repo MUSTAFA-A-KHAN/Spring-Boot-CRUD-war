b19d3dfbffbcaec5b11dfe5160c47a0af42e8613
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2024-02-12 10:26:11 +0530

    done

---
+++ src/main/java/com/tbp/crud/HelloWorldApplication.java
@@ -0,0 +1,39 @@
+//package com.tbp.crud;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.tbp.crud.entity.User;
+//import com.tbp.crud.service.UserService;
+//
+//@RestController
+//@SpringBootApplication
+//public class HelloWorldApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(HelloWorldApplication.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(HelloWorldApplication.class);
+//    }
+//
+//
+//    @RequestMapping("/")
+//    String helloWorld() {
+//        return "Hello World! /n <b> <a href=\"/userss\">Go to Users</a>";
+//    }
+//    
+//	
+//
+//}
